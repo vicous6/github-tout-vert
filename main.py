7f3fdb242f1dca3d3240832468f8186491921058
vicous <vicous@free.fr>
2023-09-29 17:16:06 +0200

    tout verrrt

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+#!/usr/bin/env python3
 import os
 
-def make_commit(date):
+def make_commit(daysAgo):
     # créer un commit dans le répository en changeant légèrement un fichier
-    os.system(f"echo '{date}' > file")
+    os.system(f"echo '{daysAgo}' > file")
     os.system('git add file')
-    os.system(f"git commit -m '{date}'--amend --no-edit --date {date}")
+    os.system(f"git commit -m '{daysAgo}' --date relative:{daysAgo}.days.ago")
+
+def make_commits(days):
+    for i in range(days):
+        make_commit(i)
 
 
-def make_commits(start_date, end_date):
-    # for date in ....
-    # make_commit(date)
+make_commits(365)
 
-
-make_commit("10/10/2023")
+os.system("git push --force origin main")
